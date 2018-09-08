fc6bf72a85957d2e9a0143d300373c403aef353f
Han Zhang <41025882+hanwavefront@users.noreply.github.com>
2018-09-07 17:39:51 -0700

    Added WavefrontHistogram and unit tests (#5)
    * Added WavefrontHistogram and unit tests

---
+++ src/main/java/com/wavefront/sdk/entities/histograms/WavefrontHistogramImpl.java
@@ -0,0 +1,396 @@
+package com.wavefront.sdk.entities.histograms;
+
+import com.tdunning.math.stats.AVLTreeDigest;
+import com.tdunning.math.stats.Centroid;
+import com.tdunning.math.stats.TDigest;
+import com.wavefront.sdk.common.Pair;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.StampedLock;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import static java.lang.Double.NaN;
+
+/**
+ * Wavefront implementation of a histogram
+ *
+ * @author Sushant Dewan (sushant@wavefront.com).
+ */
+public class WavefrontHistogramImpl {
+  /**
+   * We support approx 100 centroids for every minute bin T-Digest distributions
+   */
+  private final static int ACCURACY = 100;
+
+  /**
+   * If a thread's bin queue has exceeded MAX_BINS number of bins (e.g., the thread has data
+   * that has yet to be reported for more than MAX_BINS number of minutes), delete the oldest bin.
+   * Defaulted to 10 because we can expect the histogram to be reported at least once every
+   * 10 minutes.
+   */
+  private final static int MAX_BINS = 10;
+
+  private final Supplier<Long> clockMillis;
+
+  /**
+   * Global concurrent list of thread local histogramBinsList wrapped in WeakReference.
+   * This list holds all the thread local List of Minute Bins.
+   * This is ConcurrentLinkedDeque so that we can support 'flatMap(List::stream)' without
+   * worrying about ConcurrentModificationException.
+   * The MinuteBin itself is not thread safe and can change but it is still thread safe since we
+   * don’t ever update a bin that’s old or flush a bin that’s within the current minute.
+   */
+  private final List<WeakReference<ConcurrentLinkedDeque<MinuteBin>>> globalHistogramBinsList =
+      new ArrayList<>();
+
+  private final StampedLock stampedLock = new StampedLock();
+  // Protects read access to globalHistogramBinsList
+  private final Lock readLock = stampedLock.asReadLock();
+
+  // Protects write access to globalHistogramBinsList
+  private final Lock writeLock = stampedLock.asWriteLock();
+
+  /**
+   * ThreadLocal histogramBinsList where the initial value set is also added to a
+   * global list of thread local histogramBinsList wrapped in WeakReference
+   */
+  private final ThreadLocal<ConcurrentLinkedDeque<MinuteBin>> histogramBinsList =
+      ThreadLocal.withInitial(() -> {
+        ConcurrentLinkedDeque<MinuteBin> sharedBinsInstance = new ConcurrentLinkedDeque<>();
+        try {
+          writeLock.lock();
+          globalHistogramBinsList.add(new WeakReference<>(sharedBinsInstance));
+        } finally {
+          writeLock.unlock();
+        }
+        return sharedBinsInstance;
+  });
+
+  public WavefrontHistogramImpl() {
+    this(System::currentTimeMillis);
+  }
+
+  public WavefrontHistogramImpl(Supplier<Long> clockMillis) {
+    this.clockMillis = clockMillis;
+  }
+
+  public void update(int value) {
+    update((double) value);
+  }
+
+  public void update(long value) {
+    update((double) value);
+  }
+
+  public void update(double value) {
+    getCurrentBin().distribution.add(value);
+  }
+
+  /**
+   * Bulk-update this histogram with a set of centroids.
+   *
+   * @param means  the centroid values
+   * @param counts the centroid weights/sample counts
+   */
+  public void bulkUpdate(List<Double> means, List<Integer> counts) {
+    if (means != null && counts != null) {
+      int n = Math.min(means.size(), counts.size());
+      MinuteBin currentBin = getCurrentBin();
+      for (int i = 0; i < n; ++i) {
+        currentBin.distribution.add(means.get(i), counts.get(i));
+      }
+    }
+  }
+
+  /**
+   * @return returns the number of values in the distribution.
+   */
+  public long getCount() {
+    try {
+      readLock.lock();
+      return globalHistogramBinsList.stream().map(Reference::get).filter(Objects::nonNull).
+          flatMap(Collection::stream).mapToLong(bin -> bin.distribution.size()).sum();
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  /**
+   * @return returns the maximum value in the distribution.
+   * Returns NaN if the distribution is empty.
+   */
+  public double getMax() {
+    try {
+      readLock.lock();
+      return globalHistogramBinsList.stream().map(Reference::get).filter(Objects::nonNull).
+          flatMap(Collection::stream).
+          mapToDouble(bin -> bin.distribution.getMax()).max().orElse(NaN);
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  /**
+   * @return returns the minimum value in the distribution.
+   * Returns NaN if the distribution is empty.
+   */
+  public double getMin() {
+    try {
+      readLock.lock();
+      return globalHistogramBinsList.stream().map(Reference::get).filter(Objects::nonNull).
+          flatMap(Collection::stream).
+          mapToDouble(bin -> bin.distribution.getMin()).min().orElse(NaN);
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  /**
+   * @return returns the mean of the values in the distribution.
+   * Returns NaN if the distribution is empty.
+   */
+  public double getMean() {
+    final List<Centroid> centroids = new ArrayList<>();
+    try {
+      readLock.lock();
+      globalHistogramBinsList.stream().map(Reference::get).filter(Objects::nonNull).
+          flatMap(Collection::stream).
+          forEach(bin -> centroids.addAll(bin.distribution.centroids()));
+    } finally {
+      readLock.unlock();
+    }
+    return centroids.size() == 0 ? NaN : centroids.stream().
+        mapToDouble(c -> (c.count() * c.mean()) / centroids.size()).sum();
+  }
+
+  /**
+   * @return returns the sum of the values in the distribution.
+   */
+  public double getSum() {
+    List<Centroid> centroids = new ArrayList<>();
+    try {
+      readLock.lock();
+      globalHistogramBinsList.stream().map(Reference::get).filter(Objects::nonNull).
+          flatMap(Collection::stream).
+          forEach(bin -> centroids.addAll(bin.distribution.centroids()));
+    } finally {
+      readLock.unlock();
+    }
+    return centroids.stream().mapToDouble(c -> c.count() * c.mean()).sum();
+  }
+
+  /**
+   * Not supported, hence return Double.NaN
+   *
+   * @return stdDev
+   */
+  public double stdDev() {
+    return Double.NaN;
+  }
+
+  /**
+   * Aggregates all the minute bins prior to the current minute (because threads might be
+   * updating the current minute bin while the method is invoked) and returns a list of the
+   * distributions held within each bin. Note that invoking this method will also clear all data
+   * from the aggregated bins, thereby changing the state of the system and preventing data from
+   * being flushed more than once.
+   *
+   * @return returns a list of distributions, each a {@link Distribution} holding a timestamp
+   * as well as a list of centroids. Each centroid is a tuple containing the centroid value and
+   * count.
+   */
+  public List<Distribution> flushDistributions() {
+    final long cutoffMillis = currentMinuteMillis();
+    try {
+      writeLock.lock();
+      return processGlobalHistogramBinsList(cutoffMillis);
+    } finally {
+      writeLock.unlock();
+    }
+  }
+
+  private List<Distribution> processGlobalHistogramBinsList(long cutoffMillis) {
+    final List<Distribution> distributions = new ArrayList<>();
+    Iterator<WeakReference<ConcurrentLinkedDeque<MinuteBin>>> globalBinsIter =
+        globalHistogramBinsList.iterator();
+    while (globalBinsIter.hasNext()) {
+      WeakReference<ConcurrentLinkedDeque<MinuteBin>> weakRef = globalBinsIter.next();
+      ConcurrentLinkedDeque<MinuteBin> sharedBinsInstance = weakRef.get();
+      if (sharedBinsInstance == null) {
+        // Weak reference already garbage collected, hence remove the weakRef from global list
+        globalBinsIter.remove();
+        continue;
+      }
+
+      Iterator<MinuteBin> binsIter = sharedBinsInstance.iterator();
+      while (binsIter.hasNext()) {
+        MinuteBin minuteBin = binsIter.next();
+        if (minuteBin.minuteMillis < cutoffMillis) {
+          List<Pair<Double, Integer>> centroids = minuteBin.distribution.centroids().stream().
+              map(c -> new Pair<>(c.mean(), c.count())).collect(Collectors.toList());
+          distributions.add(new Distribution(minuteBin.minuteMillis, centroids));
+          binsIter.remove();
+        }
+      }
+    }
+    return distributions;
+  }
+
+  /**
+   * @return returns a statistical {@link Snapshot} of the histogram distribution.
+   */
+  public Snapshot getSnapshot() {
+    final TDigest snapshot = new AVLTreeDigest(ACCURACY);
+    try {
+      readLock.lock();
+      globalHistogramBinsList.stream().map(Reference::get).filter(Objects::nonNull).
+          flatMap(Collection::stream).forEach(bin -> snapshot.add(bin.distribution));
+    } finally {
+      readLock.unlock();
+    }
+
+    return new Snapshot(snapshot);
+  }
+
+  private long currentMinuteMillis() {
+    return (clockMillis.get() / 60000L) * 60000L;
+  }
+
+  /**
+   * Helper to retrieve the current bin. Will be invoked on the thread local histogramBinsList.
+   */
+  private MinuteBin getCurrentBin() {
+    ConcurrentLinkedDeque<MinuteBin> sharedBinsInstance = histogramBinsList.get();
+    long currMinuteMillis = currentMinuteMillis();
+    if (sharedBinsInstance.isEmpty() ||
+        sharedBinsInstance.getLast().minuteMillis != currMinuteMillis) {
+      sharedBinsInstance.add(new MinuteBin(ACCURACY, currMinuteMillis));
+      if (sharedBinsInstance.size() > MAX_BINS) {
+        sharedBinsInstance.removeFirst();
+      }
+    }
+    return sharedBinsInstance.getLast();
+  }
+
+  /**
+   * Wrapper for TDigest distribution
+   */
+  public static class Snapshot {
+    private final TDigest distribution;
+
+    Snapshot(TDigest dist) {
+      this.distribution = dist;
+    }
+
+    /**
+     * @return returns the number of values in the distribution.
+     */
+    public long getCount() {
+      return distribution.size();
+    }
+
+    /**
+     * @return returns the maximum value in the distribution.
+     * Returns NaN if the distribution is empty.
+     */
+    public double getMax() {
+      double max = distribution.getMax();
+      return max == Double.NEGATIVE_INFINITY ? NaN : max;
+    }
+
+    /**
+     * @return returns the minimum value in the distribution.
+     * Returns NaN if the distribution is empty.
+     */
+    public double getMin() {
+      double min = distribution.getMin();
+      return min == Double.POSITIVE_INFINITY ? NaN : min;
+    }
+
+    /**
+     * @return returns the mean of the values in the distribution.
+     * Returns NaN if the distribution is empty.
+     */
+    public double getMean() {
+      Collection<Centroid> centroids = distribution.centroids();
+      return centroids.size() == 0 ? NaN : centroids.stream()
+          .mapToDouble(c -> (c.count() * c.mean()) / centroids.size()).sum();
+    }
+
+    /**
+     * @return returns the sum of the values in the distribution.
+     */
+    public double getSum() {
+      return distribution.centroids().stream().mapToDouble(c -> c.count() * c.mean()).sum();
+    }
+
+    /**
+     * @param quantile  a given quantile, between 0 and 1
+     * @return returns the value in the distribution at the given quantile.
+     * Returns NaN if the distribution is empty.
+     */
+    public double getValue(double quantile) {
+      return distribution.quantile(quantile);
+    }
+
+    /**
+     * Returns the size of the snapshot
+     *
+     * @return size of the snapshot
+     */
+    public int getSize() {
+      return (int)distribution.size();
+    }
+  }
+
+  /**
+   * Representation of a histogram distribution, containing a timestamp and a list of centroids.
+   */
+  public static class Distribution {
+    /**
+     * The timestamp in milliseconds since the epoch.
+     */
+    public final long timestamp;
+
+    /**
+     * The list of histogram points, each a 2-dimensional {@link Pair} where the first dimension
+     * is the mean value (Double) of the centroid and second dimension is the count of points in
+     * that centroid.
+     */
+    public final List<Pair<Double, Integer>> centroids;
+
+    public Distribution(long timestamp, List<Pair<Double, Integer>> centroids) {
+      this.timestamp = timestamp;
+      this.centroids = centroids;
+    }
+  }
+
+  /**
+   * Representation of a bin that holds histogram data for a particular minute in time.
+   */
+  private static class MinuteBin {
+    /**
+     * The histogram data for the minute bin, represented as a {@link TDigest} distribution
+     */
+    public final TDigest distribution;
+
+    /**
+     * The timestamp at the start of the minute.
+     */
+    public final long minuteMillis;
+
+    MinuteBin(int accuracy, long minuteMillis) {
+      distribution = new AVLTreeDigest(accuracy);
+      this.minuteMillis = minuteMillis;
+    }
+  }
+}
---
+++ src/test/java/com/wavefront/sdk/entities/histograms/WavefrontHistogramImplTest.java
@@ -0,0 +1,190 @@
+package com.wavefront.sdk.entities.histograms;
+
+import com.wavefront.sdk.common.Pair;
+import com.wavefront.sdk.entities.histograms.WavefrontHistogramImpl.Distribution;
+import com.wavefront.sdk.entities.histograms.WavefrontHistogramImpl.Snapshot;
+
+import org.hamcrest.collection.IsMapContaining;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+
+import static java.lang.Double.NaN;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Basic unit tests around {@link WavefrontHistogramImpl}
+ *
+ * @author Han Zhang (zhanghan@vmware.com)
+ */
+public class WavefrontHistogramImplTest {
+
+  private static final double DELTA = 1e-1;
+
+  private static AtomicLong clock;
+
+  private static WavefrontHistogramImpl pow10, inc100, inc1000;
+
+  private static WavefrontHistogramImpl createPow10Histogram(Supplier<Long> clockMillis) {
+    WavefrontHistogramImpl wh = new WavefrontHistogramImpl(clockMillis);
+    wh.update(0.1);
+    wh.update(1);
+    wh.update(10);
+    wh.update(10);
+    wh.update(100);
+    wh.update(1000);
+    wh.update(10000);
+    wh.update(10000);
+    wh.update(100000);
+
+    return wh;
+  }
+
+  private Map<Double, Integer> distributionToMap(List<Distribution> distributions) {
+    Map<Double, Integer> map = new HashMap<>();
+
+    for (Distribution distribution : distributions) {
+      for (Pair<Double, Integer> centroid : distribution.centroids) {
+        map.put(centroid._1, map.getOrDefault(centroid._1, 0) + centroid._2);
+      }
+    }
+
+    return map;
+  }
+
+  @BeforeClass
+  public static void setUp() {
+    clock = new AtomicLong(System.currentTimeMillis());
+
+    // WavefrontHistogramImpl with values that are powers of 10
+    pow10 = createPow10Histogram(clock::get);
+
+    // WavefrontHistogramImpl with a value for each integer from 1 to 100
+    inc100 = new WavefrontHistogramImpl(clock::get);
+    for (int i = 1; i <= 100; i++) {
+      inc100.update(i);
+    }
+
+    // WavefrontHistogramImpl with a value for each integer from 1 to 1000
+    inc1000 = new WavefrontHistogramImpl(clock::get);
+    for (int i = 1; i <= 1000; i++) {
+      inc1000.update(i);
+    }
+
+    // Simulate that 1 min has passed so that values prior to the current min are ready to be read
+    clock.addAndGet(60000L + 1);
+  }
+
+  @Test
+  public void testDistribution() {
+    WavefrontHistogramImpl wh = createPow10Histogram(clock::get);
+    clock.addAndGet(60000L + 1);
+
+    List<Distribution> distributions = wh.flushDistributions();
+    Map<Double, Integer> map = distributionToMap(distributions);
+
+    assertEquals(7, map.size());
+    assertThat(map, IsMapContaining.hasEntry(0.1, 1));
+    assertThat(map, IsMapContaining.hasEntry(1.0, 1));
+    assertThat(map, IsMapContaining.hasEntry(10.0, 2));
+    assertThat(map, IsMapContaining.hasEntry(100.0, 1));
+    assertThat(map, IsMapContaining.hasEntry(1000.0, 1));
+    assertThat(map, IsMapContaining.hasEntry(10000.0, 2));
+    assertThat(map, IsMapContaining.hasEntry(100000.0, 1));
+
+    // check that the histogram has been cleared
+    assertEquals(0, wh.getCount());
+    assertEquals(NaN, wh.getMax(), DELTA);
+    assertEquals(NaN, wh.getMin(), DELTA);
+    assertEquals(NaN, wh.getMean(), DELTA);
+    assertEquals(0, wh.getSum(), DELTA);
+
+    Snapshot snapshot = wh.getSnapshot();
+    assertEquals(0, snapshot.getCount());
+    assertEquals(NaN, snapshot.getMax(), DELTA);
+    assertEquals(NaN, snapshot.getMin(), DELTA);
+    assertEquals(NaN, snapshot.getMean(), DELTA);
+    assertEquals(NaN, snapshot.getValue(0.5), DELTA);
+    assertEquals(0, snapshot.getSum(), DELTA);
+  }
+
+  @Test
+  public void testBulkUpdate() {
+    WavefrontHistogramImpl wh = new WavefrontHistogramImpl(clock::get);
+    wh.bulkUpdate(Arrays.asList(24.2, 84.35, 1002.0), Arrays.asList(80, 1, 9));
+    clock.addAndGet(60000L + 1);
+
+    List<Distribution> distributions = wh.flushDistributions();
+    Map<Double, Integer> map = distributionToMap(distributions);
+
+    assertEquals(3, map.size());
+    assertThat(map, IsMapContaining.hasEntry(24.2, 80));
+    assertThat(map, IsMapContaining.hasEntry(84.35, 1));
+    assertThat(map, IsMapContaining.hasEntry(1002.0, 9));
+  }
+
+  @Test
+  public void testCount() {
+    assertEquals(9, pow10.getCount());
+    assertEquals(9, pow10.getSnapshot().getCount());
+  }
+
+  @Test
+  public void testMax() {
+    assertEquals(100000, pow10.getMax(), DELTA);
+    assertEquals(100000, pow10.getSnapshot().getMax(), DELTA);
+  }
+
+  @Test
+  public void testMin() {
+    assertEquals(1, inc100.getMin(), DELTA);
+    assertEquals(1, inc100.getSnapshot().getMin(), DELTA);
+  }
+
+  @Test
+  public void testMean() {
+    assertEquals(13457.9, pow10.getMean(), DELTA);
+    assertEquals(13457.9, pow10.getSnapshot().getMean(), DELTA);
+  }
+
+  @Test
+  public void testSum() {
+    assertEquals(121121.1, pow10.getSum(), DELTA);
+    assertEquals(121121.1, pow10.getSnapshot().getSum(), DELTA);
+  }
+
+  @Test
+  public void testSize() {
+    assertEquals(9, pow10.getSnapshot().getSize());
+    assertEquals(100, inc100.getSnapshot().getSize());
+    assertEquals(1000, inc1000.getSnapshot().getSize());
+  }
+
+  @Test
+  public void testStdDev() {
+    assertEquals(NaN, pow10.stdDev(), DELTA);
+    assertEquals(NaN, inc100.stdDev(), DELTA);
+    assertEquals(NaN, inc1000.stdDev(), DELTA);
+  }
+
+  @Test
+  public void testQuantile() {
+    assertEquals(100, pow10.getSnapshot().getValue(.5), DELTA);
+
+    Snapshot snapshot = inc100.getSnapshot();
+    assertEquals(25.5, snapshot.getValue(.25), DELTA);
+    assertEquals(75.5, snapshot.getValue(.75), DELTA);
+    assertEquals(95.5, snapshot.getValue(.95), DELTA);
+    assertEquals(98.5, snapshot.getValue(.98), DELTA);
+    assertEquals(99.5, snapshot.getValue(.99), DELTA);
+
+    assertEquals(999.5, inc1000.getSnapshot().getValue(.999), DELTA);
+  }
+}
