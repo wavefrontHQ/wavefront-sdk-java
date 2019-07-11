b4fe2ab5bb19a7ff203c4c988c3d238464158fe4
Mike McMahon <mike.mcmahon@wavefront.com>
2019-07-11 08:42:42 -0700

    remove unimplemented iterable methods

--- src/main/java/com/wavefront/sdk/common/clients/exceptions/MultiClientIOException.java
+++ src/main/java/com/wavefront/sdk/common/clients/exceptions/MultiClientIOException.java
@@ -1,14 +1,10 @@
 package com.wavefront.sdk.common.clients.exceptions;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.io.IOException;
 import java.lang.Iterable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Spliterator;
-import java.util.function.Consumer;
 
 public class MultiClientIOException implements Iterable<IOException>
 {
@@ -17,16 +13,6 @@ public class MultiClientIOException implements Iterable<IOException>
     @Override
     public Iterator<IOException> iterator() {
         return exceptions.iterator();
-    }
-
-    @Override
-    public void forEach(Consumer<? super IOException> action) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Spliterator<IOException> spliterator() {
-        throw new NotImplementedException();
     }
 
     public void add(IOException exception) {
