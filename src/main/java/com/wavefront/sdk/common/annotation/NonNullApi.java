37da824114072a548393d18a4b429f7119ccc520
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2019-07-12 11:46:04 -0700

    Add @NonNullApi to set package level non-null semantics (#89)
    To support nullability enforcement, introduce @NonNullApi annotation
    that applies to package level. This sets default non-nullable
    semantics to the return types and parameters of classes in the annotated
    package.

---
+++ src/main/java/com/wavefront/sdk/common/annotation/NonNullApi.java
@@ -0,0 +1,26 @@
+package com.wavefront.sdk.common.annotation;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A common annotation to declare that parameters and return values
+ * are to be considered as non-nullable by default for a given package.
+ * <p>
+ * Annotate package declaration in "package-info.java" files, then it
+ * sets the non-nullable semantics to the package.
+ *
+ * @author Tadaya Tsuyukubo
+ */
+@Target(ElementType.PACKAGE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Nonnull
+@TypeQualifierDefault({ElementType.METHOD, ElementType.PARAMETER})
+public @interface NonNullApi {
+}
