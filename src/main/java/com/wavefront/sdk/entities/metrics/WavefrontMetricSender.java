d1cdac02d918f0160fe087a879f308632a7718f5
sushantdewan123 <sushant@wavefront.com>
2020-08-13 20:53:31 -0700

    Add the ability to send delta counters with timestamp

--- src/main/java/com/wavefront/sdk/entities/metrics/WavefrontMetricSender.java
+++ src/main/java/com/wavefront/sdk/entities/metrics/WavefrontMetricSender.java
@@ -66,4 +66,33 @@ public interface WavefrontMetricSender {
       sendMetric(name, value, null, source, tags);
     }
   }
+
+  /**
+   * Sends the given delta counter to Wavefront. The timestamp for the point on the client side is
+   * null because the final timestamp of the delta counter is assigned when the point is
+   * aggregated on the server side. Do not use this method to send older points (say around 5 min
+   * old) as they will be aggregated on server with the current timestamp which yields in a wrong
+   * final aggregated value.
+   *
+   * @param name      The name of the delta counter. Name will be prefixed by ∆ if it does
+   *                  not start with that symbol already. Also, spaces are replaced with '-'
+   *                  (dashes) and quotes will be automatically escaped.
+   * @param value     The delta value to be sent. This will be aggregated on the Wavefront server
+   *                  side.
+   * @param timestamp The timestamp in milliseconds since the epoch to be sent. If null then the
+   *    *             timestamp is assigned by Wavefront when data is received.
+   * @param source    The source (or host) that's sending the metric. If null then assigned by
+   *                  Wavefront.
+   * @param tags      The tags associated with this metric.
+   * @throws IOException if there was an error sending the metric.
+   */
+  default void sendDeltaCounter(String name, double value, @Nullable Long timestamp, @Nullable String source,
+                                @Nullable Map<String, String> tags) throws IOException {
+    if (!name.startsWith(DELTA_PREFIX) && !name.startsWith(DELTA_PREFIX_2)) {
+      name = DELTA_PREFIX + name;
+    }
+    if (value > 0) {
+      sendMetric(name, value, timestamp, source, tags);
+    }
+  }
 }
